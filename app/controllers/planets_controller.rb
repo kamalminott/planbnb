36d411139ef1f54234bd19edf0689b16f47a57e8
maaz sattar <maazsattar@gmail.com>
2022-11-21 17:25:35 +0000

    added show method, destroy and view and has manys

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -1,5 +1,15 @@
 class PlanetsController < ApplicationController
-def index
-  @planets = Planet.all
-end
+  def index
+    @planets = Planet.all
+  end
+
+  def show
+    @planet = Planet.find(params[:id])
+  end
+
+  def destroy
+    @planet = Planet.find(params[:id])
+    @planet.destroy
+    redirect_to planets_path, notice: "Planet was successfully destroyed."
+  end
 end
