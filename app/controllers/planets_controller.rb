f209099fb053abc8d6a287e78b5d76320015698f
Do Yeon Kim <doyeon1105@gmail.com>
2022-11-21 16:41:50 +0000

    edit and update method for planet controller

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -2,4 +2,19 @@ class PlanetsController < ApplicationController
 def index
   @planets = Planet.all
 end
+
+def edit
+  @planet = Planet.find(params[:id])
+end
+
+def update
+  @planet = Planet.find(params[:id])
+  @planet.update(planet_params)
+  redirect_to planet_path(@planet)
+end
+
+private
+
+def planet_params
+  params.require(:planet).permit(:name, :address, :description, :price)
 end
