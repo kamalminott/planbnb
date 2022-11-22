7a3e22ed80da24131ebcd1ef7b1a7d203abd8c4a
Do Yeon Kim <doyeon1105@gmail.com>
2022-11-22 10:13:14 +0000

    little work for the new and create method

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -15,9 +15,9 @@ class PlanetsController < ApplicationController
   def create
     @planet = Planet.new(planet_params)
     if @planet.save
-      redirect_to @planet
+      redirect_to planet_path(@planet)
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
