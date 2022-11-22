09c858ac66aef4e98b2014c695a344521650a822
maaz sattar <maazsattar@gmail.com>
2022-11-22 09:56:51 +0000

    show page complete

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -21,27 +21,27 @@ class PlanetsController < ApplicationController
     end
   end
 
-def edit
-  @planet = Planet.find(params[:id])
-end
+  def edit
+    @planet = Planet.find(params[:id])
+  end
 
-def update
-  @planet = Planet.find(params[:id])
-  @planet.update(planet_params)
-  redirect_to planet_path(@planet)
-end
+  def update
+    @planet = Planet.find(params[:id])
+    @planet.update(planet_params)
+    redirect_to planet_path(@planet)
+  end
 
   def destroy
     @planet = Planet.find(params[:id])
     @planet.destroy
     redirect_to planets_path, notice: "Planet was successfully destroyed."
   end
-  
+
 
 private
 
-def planet_params
-  params.require(:planet).permit(:name, :address, :description, :price)
-end
+  def planet_params
+    params.require(:planet).permit(:name, :address, :description, :price)
+  end
 
-end 
+end
