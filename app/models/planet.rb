a973ae3b8d0a29ad0e3838eac24dc4e207df6da1
maaz sattar <maazsattar@gmail.com>
2022-11-24 12:23:14 +0000

    bookings crud done

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -4,4 +4,9 @@ class Planet < ApplicationRecord
   has_many :users, through: :bookings
   has_many :bookings
   has_one_attached :photo
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :photo, presence: true
+  validates :price, presence: true
 end
